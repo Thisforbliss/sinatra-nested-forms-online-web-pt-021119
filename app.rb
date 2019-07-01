1d3f0bcfe5b58e045abfe6f5590cb2ff409f0b56
Malcolm Leake <echelon1089@gmail.com>
2019-07-01 22:08:41 +0000

    Done.

--- app.rb
+++ app.rb
@@ -3,7 +3,14 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
-
+  get '/' do 
+    "Welcome to the Nested Forms Lab!"
   end
+
+  get '/new' do 
+  erb :new
+  end
+  
+  
+end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+  attr_accessor :name, :type, :booty
+  
+  Ship = []
+  
+  def initialize(params)
+  @name = params[:name]
+  @type = params[:type]
+  @booty = params[:booty]
+  Ship << self
+  end
+  
+  def self.all
+  Ship
+  end
+  
+  def self.clear
+  Ship.clear
+  end
+  
 end
